f6a38c4e300d66482cbbb39eab6f0b577530ed2c
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-27 10:57:32 -0500

    laptop

--- BioFolderMasterPro.py
+++ BioFolderMasterPro.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     list_of_files = os.listdir()
-    list_of_files.remove('BioFolderMasterPro.py')
+    list_of_files.remove('BioFolderMasterPro.py') # This makes it  so that the code won't count itself
     print(list_of_files)
 
 main()
