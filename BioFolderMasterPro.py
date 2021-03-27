e265499a82a6efa802e565cd005b9bad3373971c
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-27 11:14:31 -0500

    #

--- BioFolderMasterPro.py
+++ BioFolderMasterPro.py
@@ -1,8 +1,8 @@
-import os
+import os # one of the libraries that I needs
 
 def main():
-    list_of_files = os.listdir()
+    list_of_files = os.listdir() # this make a list of the files in the main folder
     list_of_files.remove('BioFolderMasterPro.py') # This makes it  so that the code won't count itself
-    print(list_of_files)
+    print(list_of_files) # just testing for now
 
 main()
